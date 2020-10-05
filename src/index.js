57149affcfbbff0ec3b41e4d0640924162909c41
TatyanaBojarkina <galaktozat@yandex.ru>
2020-10-05 11:39:20 +0400

    feat: update index.js Signed-off-by: TatyanaBojarkina <galaktozat@yandex.ru>

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function reverse (n) {
-  
+	n = Math.abs(n);
+  n = n.toString();
+  let arr = n.split('');
+  let arr1 = '';
+  for(let i = 0; i < arr.length; i++)
+  {
+	  arr1=arr1 + arr[arr.length-1-i];
+  }
+  return arr1;
+
 }
